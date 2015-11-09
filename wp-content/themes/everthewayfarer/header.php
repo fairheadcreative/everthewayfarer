ac57c6a73636c1f85343c281d61fe4a54acf5420
Adam Fairhead <adam@adamfairhead.com>
2015-11-09 12:03:15 -0500

    Include additional analytics

--- wp-content/themes/everthewayfarer/header.php
+++ wp-content/themes/everthewayfarer/header.php
@@ -9,6 +9,7 @@
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css" />
   <script src="//use.typekit.net/zif1rom.js"></script>
   <script>try{Typekit.load();}catch(e){}</script>
+  <script>(function(h,o,t,j,a,r){ h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)}; h._hjSettings={hjid:100584,hjsv:5}; a=o.getElementsByTagName('head')[0]; r=o.createElement('script');r.async=1; r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv; a.appendChild(r); })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');</script>
   <?php wp_head(); ?>
 </head>
 
