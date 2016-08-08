a80aca4ab081f8d2763c8fba3890c1a6f3aedc8a
dcerimagic <denis.cerimagic@fairheadcreative.com>
2016-08-08 08:22:19 +0200

    Added new subscribe page/splash build

--- wp-content/themes/everthewayfarer/page-subscribe.php
+++ wp-content/themes/everthewayfarer/page-subscribe.php
@@ -1,53 +1,7 @@
 <?php get_header(); ?>
 
-<article class="single is-subscribe">
-  <section id="main group">
-    <div class="container">
-      <header>
-        <h1>Subscribe to my blog & I’ll send you a pack of free wayfaring wallpapers for your smartphone, tablet and laptop!</h1>
-      </header> 
+<!--<div style="position: absolute; top:0; left: 50%;transform: translatex(-50%);bottom:0;opacity:.2;pointer-events:none;z-index:10;"><img src="<?php echo get_bloginfo('template_directory');?>/images/test.jpg" alt="" style="width: 1058px !important; max-width: 1058px !important;"></div>-->
 
-      <div id="mc_embed_signup">
-        <form action="//everthewayfarer.us10.list-manage.com/subscribe/post?u=0901f43eef09ce63f92f2de2c&amp;id=ba5fbd5025" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-            <div id="mc_embed_signup_scroll">
-          
-          <div class="email-address">
-            <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Your email address" autofocus>
-          </div>
-          <div id="mce-responses" class="clear">
-            <div class="response" id="mce-error-response" style="display:none"></div>
-            <div class="response" id="mce-success-response" style="display:none"></div>
-          </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-            <div style="position: absolute; left: -5000px;"><input type="text" name="b_0901f43eef09ce63f92f2de2c_ba5fbd5025" tabindex="-1" value=""></div>
-            <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
-          </div>
-        </form> 
-      </div>
-
-      <figure class="subscribe-devices"></figure>
-    </div>
-  </section>
-</article>
-
-<ul class="slideshow-splash">
-	<li>
-		<span>Ernst Tinaja</span>
-	</li>
-	<li>
-		<span>Cotton in the Big Bend</span>
-	</li>
-	<li>
-		<span>National Park Crawl</span>
-	</li>
-	<li>
-		<span>Northern Nights, Northern Lights</span>
-	</li>
-	<li>
-		<span>Ice Cave</span>
-	</li>
-	<li>
-		<span>Iceland Horses</span>
-	</li>
-</ul>
+<?php get_template_part( 'includes/subscribe-splash' ); ?>
 
 <?php get_footer(); ?>
