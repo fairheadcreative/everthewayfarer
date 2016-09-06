c64abecc3892e6ed6ed051e67d442a8a448c7a29
Dean <immigrantsheep@live.com>
2016-09-06 23:48:11 +0200

    Added modifications:
    - new text in navigation menu
    - started adding new header for the "National Parks" category
    - added location field for Postcards

--- wp-content/themes/everthewayfarer/header.php
+++ wp-content/themes/everthewayfarer/header.php
@@ -27,15 +27,15 @@
            </span>
             <ul>
               <li class="nav-primary-1 <?php if (!is_home() && is_category('Postcard') || !is_home() && has_category('Postcards')){ echo 'active'; };?>"><a href="<?php echo site_url(); ?>/postcards/">Latest Postcards</a></li>
-              <li class="nav-primary-1 <?php if (!is_home() && is_category('Journey') || !is_home() && has_category('Journey')){ echo 'active'; };?>"><a href="<?php echo site_url(); ?>/journey/">Journeys</a></li>
-              <!--<li class="nav-primary-1 <?php if (!is_home() && is_category('National Parks') || !is_home() && has_category('National Parks')){ echo 'active'; };?>"><a href="<?php echo site_url(); ?>/nationalparks/">Park Crawl</a></li>-->
+              <li class="nav-primary-1 <?php if (!is_home() && is_category('National Parks') || !is_home() && has_category('National Parks')){ echo 'active'; };?>"><a href="<?php echo site_url(); ?>/nationalparks/">Park Crawl</a></li>
               <li class="nav-primary-1 <?php if (!is_home() && is_category('News') || !is_home() && has_category('News')){ echo 'active'; };?>"><a href="<?php echo site_url(); ?>/news/">News</a></li>
-              <li class="nav-primary-1 <?php if (!is_home() && is_category('Gear') || !is_home() && has_category('Gear')) { echo 'active'; };?>"><a href="<?php echo site_url(); ?>/gear/">Gear &amp; Reviews</a></li>
+              <li class="nav-primary-1 <?php if (!is_home() && is_category('Gear') || !is_home() && has_category('Gear')) { echo 'active'; };?>"><a href="<?php echo site_url(); ?>/gear/">Travel Gear</a></li>
+              <li class="nav-primary-1 <?php if (!is_home() && is_category('Journey') || !is_home() && has_category('Journey')){ echo 'active'; };?>"><a href="<?php echo site_url(); ?>/journey/">Journeys</a></li>
+              <li class="nav-primary-2 <?php if (!is_home() && is_page('about')){ echo 'active'; };?>"><a href="<?php echo site_url(); ?>/about/">About Siv</a></li>
               <li class="nav-primary-1"><a href="http://www.suntrailimages.com">Photography</a></li>
-              <li class="nav-primary-2 <?php if (!is_home() && is_page('about')){ echo 'active'; };?>"><a href="<?php echo site_url(); ?>/about/">About</a></li>
-              <li class="nav-primary-2 <?php if (!is_home() && is_page('resources')){ echo 'active'; };?>"><a href="<?php echo site_url(); ?>/resources/">Resources</a></li>
+              <!--<li class="nav-primary-2 <?php if (!is_home() && is_page('resources')){ echo 'active'; };?>"><a href="<?php echo site_url(); ?>/resources/">Resources</a></li>-->
+              <!--<li class="nav-primary-1 <?php if (!is_home() && is_page('subscribe')){ echo 'active'; };?>"><a href="<?php echo site_url(); ?>/subscribe/">Subscribe</a></li>-->
               <li class="nav-primary-1 <?php if (is_home()){ echo 'active'; };?>"><a href="<?php echo site_url(); ?>">Homepage</a></li>
-              <li class="nav-primary-1 <?php if (!is_home() && is_page('subscribe')){ echo 'active'; };?>"><a href="<?php echo site_url(); ?>/subscribe/">Subscribe</a></li>
               <li class="nav-icons">
                 <a class="nav-primary-twitter" href="https://twitter.com/WayfaringSiv" target="_blank">
                   <span class="icon-block">
--- wp-content/themes/everthewayfarer/index.php
+++ wp-content/themes/everthewayfarer/index.php
@@ -53,6 +53,12 @@ if ( is_home() ) { ?>
         </a>
       </div>
     <?php };*/ ?>
+              <?php if (in_category('nationalparks')) { ?>
+<h1>National Park Crawl</h1>
+<h3>Description goes here, about why I went on this crawl, and what<br>
+it represents for the world of ethical travel.</h3>
+<a class="button" href="">Share on Facebook</a><a class="button" href="">Share on Twitter</a>
+          <?php } ?>
 
     <div class="articles<?php if ( is_home() ) {?> articles-mixed<?php } ?>">
       <?php # If it's the news category, add a title
