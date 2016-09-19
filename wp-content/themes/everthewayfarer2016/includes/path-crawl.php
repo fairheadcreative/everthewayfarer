3fcf1375c80b484e465f707fc98a05af0568fb91
dcerimagic <denis.cerimagic@fairheadcreative.com>
2016-09-19 14:12:25 +0200

    Modified Park Crawl paths

--- wp-content/themes/everthewayfarer2016/includes/path-crawl.php
+++ wp-content/themes/everthewayfarer2016/includes/path-crawl.php
@@ -1,8 +1,8 @@
 <span class="path-crawl">
-  <svg class="path-full" width="600" height="300">
-    <path d="M10,290 Q300,0 560,300" fill="none" stroke="#C1C1C1" stroke-width="15px" stroke-linecap="round" stroke-dasharray="1, 40" />
+  <svg class="path-full" width="600" height="400" preserveAspectRatio="none" viewBox="0 0 600 400">
+    <path d="M10,200 Q400,0 530,390" fill="none" stroke="#C1C1C1" stroke-height="15px" stroke-width="15px" stroke-linecap="round" stroke-dasharray="1, 40"></path>
   </svg>
-  <svg class="path-quarter" width="600" height="300">
-    <path d="M10,290 Q250,100 390,300" fill="none" stroke="#C1C1C1" stroke-width="15px" stroke-linecap="round" stroke-dasharray="1, 40" />
+  <svg class="path-half" width="400" height="400" preserveAspectRatio="none" viewBox="0 0 400 400">
+    <path d="M10,200 Q280,0 345,390" fill="none" stroke="#C1C1C1" stroke-height="15px" stroke-width="15px" stroke-linecap="round" stroke-dasharray="1, 40"></path>
   </svg>
 </span>
