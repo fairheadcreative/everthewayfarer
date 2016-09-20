dfa31577300589f783225958b0bb671687f20adf
dcerimagic <denis.cerimagic@fairheadcreative.com>
2016-09-20 10:59:46 +0200

    Fine-tuned nav and Park Crawl paths

--- wp-content/themes/everthewayfarer2016/includes/path-crawl.php
+++ wp-content/themes/everthewayfarer2016/includes/path-crawl.php
@@ -1,8 +1,8 @@
 <span class="path-crawl">
   <svg class="path-full" width="600" height="400" preserveAspectRatio="none" viewBox="0 0 600 400">
-    <path d="M10,180 Q400,0 530,370" vector-effect="non-scaling-stroke" fill="none" stroke="#C1C1C1" stroke-height="15px" stroke-width="15px" stroke-linecap="round" stroke-dasharray="1, 40"></path>
+    <path d="M15,180 Q400,0 530,370" vector-effect="non-scaling-stroke" fill="none" stroke="#C1C1C1" stroke-height="15px" stroke-width="15px" stroke-linecap="round" stroke-dasharray="1, 40"></path>
   </svg>
   <svg class="path-half" width="400" height="400" preserveAspectRatio="none" viewBox="0 0 400 400">
-    <path d="M10,170 Q300,0 345,370" vector-effect="non-scaling-stroke" fill="none" stroke="#C1C1C1" stroke-height="15px" stroke-width="15px" stroke-linecap="round" stroke-dasharray="1, 40"></path>
+    <path d="M10,180 Q300,0 345,370" vector-effect="non-scaling-stroke" fill="none" stroke="#C1C1C1" stroke-height="15px" stroke-width="15px" stroke-linecap="round" stroke-dasharray="1, 40"></path>
   </svg>
 </span>
