fcfdb6d03f79c7b1cab740c564247a63e93644ce
Dean Gvozdic <dean.gvozdic@gmail.com>
2016-12-01 16:32:38 +0100

    Added the Featured Image from each post into the RSS feed

--- wp-content/themes/everthewayfarer2016/functions.php
+++ wp-content/themes/everthewayfarer2016/functions.php
@@ -55,5 +55,15 @@ function the_category_unlinked($separator = ' ') {
     echo $thelist;
 }
 
+//add featured images to rss feed
+function wcs_post_thumbnails_in_feeds( $content ) {
+    global $post;
+    if( has_post_thumbnail( $post->ID ) ) {
+        $content = '<p>' . get_the_post_thumbnail( $post->ID ) . '</p>' . $content;
+    }
+    return $content;
+}
+add_filter( 'the_excerpt_rss', 'wcs_post_thumbnails_in_feeds' );
+add_filter( 'the_content_feed', 'wcs_post_thumbnails_in_feeds' );
 
 ?>
